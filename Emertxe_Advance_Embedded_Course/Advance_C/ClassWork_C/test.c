f52d6552d577493883284e45e606b0596fefbdde
GulamMustafa7 <g.kazi2511@gmail.com>
2024-06-13 23:03:08 +0530

    Added test.c file for testing

---
+++ Emertxe_Advance_Embedded_Course/Advance_C/ClassWork_C/test.c
@@ -0,0 +1,21 @@
+//########################################################################################//
+
+/*
+* Name:         GulamMustafa Ibrahim Kazi.
+* Date:
+* Description:
+* Sample input:
+* Sample output:
+*/
+
+//########################################################################################//
+
+#include<stdio.h>
+
+int main()
+{
+	printf("For git testing\n");
+
+	return 0;
+}
+
